4410dd1cbe7908b131800aaafbaee8c0364b0f22
xGLUEx <education@likeglue.org>
2024-11-11 14:43:59 +0100

    Update bulles.lua
    Aide C.Scheyder

--- bulles.lua
+++ bulles.lua
@@ -34,16 +34,19 @@ function creeBulles(pX, pY, pR, pS, pDX, pDY)
 end
 
 function ajoutBulles()
-    for i=1,#bulles do
-        local object = bulles[i]
-        object.r = math.random(5,50)
-        object.x = math.random(object.r, largeur - object.r)
-        object.y = math.random(object.r, hauteur - object.r)
-        object.speed = math.random(5,40)
-        object.dx = math.random(1,10)
-        object.dy = math.random(1,10)
-        
-        table.insert(bulles, creeBulles(object.x, object.y, object.r, object.speed, object.dx, object.dy))
-    end
-    table.insert(bulles, creeBulles(200, 400, 20, 30, 5, 6)) -- "LA FONCTION NE MARCHE PAS AVEC KEYPRESSED SANS CREER CETTE INSERTION"
+    -- Version décomposée des expressions pour les paramètres
+    local object = {}
+    object.r = math.random(5,50)
+    object.x = math.random(object.r, largeur - object.r)
+    object.y = math.random(object.r, hauteur - object.r)
+    object.speed = math.random(5,40)
+    object.dx = math.random(1,10)
+    object.dy = math.random(1,10)
+
+    table.insert(bulles, creeBulles(object.x, object.y, object.r, object.speed, object.dx, object.dy))
+
+    -- Version condensée des expressions pour les paramètres
+    --local r = math.random(5,50)
+    --table.insert(bulles, creeBulles(math.random(r, largeur - r), math.random(r, hauteur - r), r, math.random(5,40), math.random(1,10),math.random(1,10)))
+    
 end
